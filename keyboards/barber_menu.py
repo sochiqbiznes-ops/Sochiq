32248efe2b58ff4fa4a8eea41f55b6ba2210315b
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-04-29 14:00:25 +0500

    Update barber_menu.py

--- keyboards/barber_menu.py
+++ keyboards/barber_menu.py
@@ -5,8 +5,7 @@ def barber_menu_kb():
     return ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="➕ Mijoz qo‘shish")],
-            [KeyboardButton(text="📦 Topshirish")],
-            [KeyboardButton(text="💰 To‘lov")],
+            [KeyboardButton(text="🗑 Mijoz o‘chirish")],
             [KeyboardButton(text="📊 Hisobot")],
             [KeyboardButton(text="🔙 Orqaga")]
         ],
