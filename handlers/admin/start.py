19f8a6a8c09ab8e91d5f14d6ce76f30c28cf80b0
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-04-29 14:13:20 +0500

    Update start.py

--- handlers/admin/start.py
+++ handlers/admin/start.py
@@ -31,3 +31,67 @@ async def start_handler(message: Message):
         "🏪 Sartaroshxonalar:",
         reply_markup=barber_inline_kb(barbers)
     )
+
+@router.message(F.text == "➕ Sartaroshxona qo‘shish")
+async def add_barber(message: Message):
+
+    user_state[message.from_user.id] = "add_barber"
+
+    await message.answer("🏪 Sartaroshxona nomini yozing:")
+
+
+@router.message()
+async def save_text(message: Message):
+
+    uid = message.from_user.id
+
+    if uid not in user_state:
+        return
+
+    state = user_state[uid]
+
+    conn = connect()
+    cur = conn.cursor()
+
+    if state == "add_barber":
+        cur.execute(
+            "INSERT INTO barbers (name) VALUES (?)",
+            (message.text,)
+        )
+        conn.commit()
+
+        del user_state[uid]
+
+        cur.execute("SELECT * FROM barbers")
+        barbers = cur.fetchall()
+
+        conn.close()
+
+        await message.answer("✅ Qo‘shildi")
+
+        await message.answer(
+            "🏪 Yangilangan ro‘yxat:",
+            reply_markup=barber_inline_kb(barbers)
+        )
+
+@router.callback_query(F.data.startswith("barber_"))
+async def open_barber(call: CallbackQuery):
+
+    barber_id = int(call.data.split("_")[1])
+
+    current_barber[call.from_user.id] = barber_id
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT name FROM barbers WHERE id = ?", (barber_id,))
+    barber = cur.fetchone()
+
+    conn.close()
+
+    await call.message.answer(
+        f"🏪 {barber[0]} ichiga kirdingiz\n\n"
+        "👉 Keyingi bosqich: mijoz system",
+    )
+
+    await call.answer()
