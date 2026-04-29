372e6ecf6e5753613e4850ff278d3440422b56af
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-04-29 12:29:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
 import asyncio
-import os
 
 from aiogram import Bot, Dispatcher
 
-from config import BOT_TOKEN, ADMIN_ID
+from config import BOT_TOKEN
 from db import init_db
+from handlers.admin import router as admin_router
 
-# HANDLERS
-from handlers import admin
-from handlers import barber
-from handlers import client
-
-
-# =========================
-# BOT INIT
-# =========================
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-
-# =========================
-# INCLUDE ROUTERS
-# =========================
-dp.include_router(admin.router)
-dp.include_router(barber.router)
-dp.include_router(client.router)
+dp.include_router(admin_router)
 
 
-# =========================
-# ADMIN FILTER (GLOBAL SIMPLE CHECK)
-# =========================
-@dp.message()
-async def global_block(message):
-    if message.from_user.id != ADMIN_ID:
-        await message.answer("⛔ Ruxsat yo‘q")
-
-
-# =========================
-# START BOT
-# =========================
 async def main():
-    await init_db()
-    print("🚀 Bot ishga tushdi...")
-
+    init_db()
     await dp.start_polling(bot)
 
 
