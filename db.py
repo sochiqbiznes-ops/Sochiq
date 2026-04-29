6e63dc4f1d93272b72f12e6afa294a1da5614372
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-04-29 12:26:17 +0500

    Update db.py

--- db.py
+++ db.py
@@ -2,14 +2,15 @@ import sqlite3
 
 DB_NAME = "crm.db"
 
+
 def connect():
     return sqlite3.connect(DB_NAME)
+
 
 def init_db():
     conn = connect()
     cur = conn.cursor()
 
-    # BARBERS
     cur.execute("""
     CREATE TABLE IF NOT EXISTS barbers (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,23 +18,11 @@ def init_db():
     )
     """)
 
-    # CLIENTS
     cur.execute("""
     CREATE TABLE IF NOT EXISTS clients (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
-        barber TEXT,
-        received INTEGER DEFAULT 0,
-        paid INTEGER DEFAULT 0,
-        price INTEGER DEFAULT 2000
-    )
-    """)
-
-    # ACCESS
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS access (
-        user_id INTEGER,
-        barber TEXT
+        barber_id INTEGER
     )
     """)
 
