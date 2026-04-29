fb5c88d076f5f4da54bd7027bf8a2b5fc911e70d
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-04-29 14:19:08 +0500

    Update __init__.py

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -5,3 +5,7 @@ router = Router()
 from .start import router as start_router
 
 router.include_router(start_router)
+
+from .client import router as client_router
+
+router.include_router(client_router)
