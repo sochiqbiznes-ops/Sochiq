9b241d6d91a4b9074a5ce6e53fbc60ab1ad85fb2
sochiqbiznes-ops <sochiqbiznes@gmail.com>
2026-05-06 14:42:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,3 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN yo‘q")
-
-if not ADMIN_ID:
-    raise ValueError("ADMIN_ID yo‘q")
